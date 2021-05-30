890bd9a08c36df57374ced1b6b498e50a4fac7b6
Vitaliy Boyarsky <boyarsky.vitaliy@live.com>
2021-05-31 01:02:21 +0300

    Configure Arduino Uno to communicate with IoT device gateway

--- iot-device/src/arduino/sketch/sketch.ino
+++ iot-device/src/arduino/sketch/sketch.ino
@@ -10,6 +10,7 @@ byte const MAC[] = { 0x90, 0xA2, 0xDA, 0x0F, 0x5A, 0x22 };
 byte const IP[] = { 192, 168, 0, 111 };
 IPAddress const DNS(192, 168, 0, 1);
 
+String DEVICE_ID = "arduino";
 byte const GATEWAY[] = { 192, 168, 0, 100 };
 int const PORT = 10000;
 
@@ -18,16 +19,48 @@ struct Telemetry {
     float humidity;
 };
 
+class DeviceGateway {
+    public:
+        DeviceGateway(String deviceId) {
+            _deviceId = deviceId;
+        };
+
+        inline bool connect(const byte *host, uint16_t port) { return _client.connect(host, port); };
+        inline bool connected() { return _client.connected(); };
+        inline void attach() { return attach(_client); };
+        inline void detach() { return detach(_client); };
+        inline void publish(Telemetry &record) { return publish(record, _client); };
+
+        void attach(Stream &stream) {
+            stream.print("att ");
+            stream.println(_deviceId);
+        };
+
+        void detach(Stream &stream) {
+            stream.print("det ");
+            stream.println(_deviceId);
+        };
+
+        void publish(Telemetry &record, Stream &stream) {
+            stream.print("pub ");
+            stream.print(record.temperature);
+            stream.print(" ");
+            stream.print(record.humidity);
+            stream.println();
+        };
+
+    private:
+        String _deviceId;
+        EthernetClient _client;
+
+};
+
 dht DHT;
-EthernetClient client;
 QueueHandle_t queue;
+DeviceGateway gateway = DeviceGateway(DEVICE_ID);
 
 void setup() {
     Serial.begin(9600);
-    while (!Serial) {
-        continue;
-    }
-
     setupEthernet();
 
     queue = xQueueCreate(10, sizeof(Telemetry));
@@ -65,9 +98,10 @@ void TaskTelemetryPublish(void * pvParameters) {
     (void) pvParameters;
 
     Serial.println("connecting...");
-    if (client.connect(GATEWAY, PORT)) {
+    if (gateway.connect(GATEWAY, PORT)) {
         Serial.println("connected");
-        client.println();
+        gateway.detach();
+        gateway.attach();
     } else {
         Serial.println("connection failed");
     }
@@ -75,10 +109,9 @@ void TaskTelemetryPublish(void * pvParameters) {
     Telemetry record;
     for (;;) {
         if (xQueueReceive(queue, &record, portMAX_DELAY) == pdPASS) {
-            String line = String(record.temperature) + " " + String(record.humidity);
-            Serial.println(line);
-            if (client.connected()) {
-                client.println(line);
+            gateway.publish(record, Serial);
+            if (gateway.connected()) {
+                gateway.publish(record);
             }
         }
     }
